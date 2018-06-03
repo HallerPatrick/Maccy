00a06d2cbd0f9f0708702e1bf04eba4ee8769064
patrickhaller <patrickhaller40@googlemail.com>
2018-06-03 14:55:09 +0200

    Initial Commit

---
+++ Time Good Wasted/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Time Good Wasted
+//
+//  Created by Patrick Haller on 03.06.18.
+//  Copyright © 2018 Patrick Haller. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Time Good Wasted/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Time Good Wasted
+//
+//  Created by Patrick Haller on 03.06.18.
+//  Copyright © 2018 Patrick Haller. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
